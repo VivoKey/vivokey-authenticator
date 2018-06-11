Yubico/yubioath-android

Prevent overwriting credentials.

app/src/main/kotlin/com/yubico/yubioath/client/OathClient.kt
@@ -1,5 +1,6 @@
 package com.yubico.yubioath.client
 
+import com.yubico.yubioath.exc.DuplicateKeyException
 import com.yubico.yubioath.exc.PasswordRequiredException
 import com.yubico.yubioath.protocol.ChallengeSigner
 import com.yubico.yubioath.protocol.CredentialData
@@ -114,6 +115,9 @@ class OathClient(backend: Backend, private val keyManager: KeyManager) : Closeab
             }
             if (oathType == OathType.TOTP && period != 30) {
                 name = "$period/$name"
+            }
+            if (api.listCredentials().contains(name)) {
+                throw DuplicateKeyException()
             }
             api.putCode(name, secret, oathType, algorithm, digits, counter, touch)
             return Credential(deviceInfo.id, name, oathType, touch)

app/src/main/kotlin/com/yubico/yubioath/exc/DuplicateKeyException.kt
@@ -1,0 +1,5 @@
+package com.yubico.yubioath.exc
+
+import java.io.IOException
+
+class DuplicateKeyException() : IOException("A Credential with the same name already exists")

app/src/main/kotlin/com/yubico/yubioath/ui/add/AddCredentialActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.yubico.yubioath.R
+import com.yubico.yubioath.exc.DuplicateKeyException
 import com.yubico.yubioath.ui.BaseActivity
 import kotlinx.coroutines.experimental.CancellationException
 import kotlinx.coroutines.experimental.android.UI
@@ -77,7 +78,9 @@ class AddCredentialActivity : BaseActivity<AddCredentialViewModel>(AddCredential
                                 }
                             })
                             finish()
-                        } catch(e: CancellationException) {
+                        } catch (e: CancellationException) {
+                        } catch (e: DuplicateKeyException) {
+                            markDuplicateName()
                         } catch (e: Exception) {
                             Log.e("yubioath", "exception", e)
                             validateVersion(data, viewModel.lastDeviceInfo.version)

VivoKey/vivokey-authenticator

app/src/main/kotlin/com/dangerousthings/vivoauth/client/OathClient.kt
@@ -1,5 +1,6 @@
 package com.dangerousthings.vivoauth.client
 
+import com.dangerousthings.vivoauth.exc.DuplicateKeyException
 import com.dangerousthings.vivoauth.exc.PasswordRequiredException
 import com.dangerousthings.vivoauth.protocol.ChallengeSigner
 import com.dangerousthings.vivoauth.protocol.CredentialData
@@ -114,6 +115,9 @@ class OathClient(backend: Backend, private val keyManager: KeyManager) : Closeab
             }
             if (oathType == OathType.TOTP && period != 30) {
                 name = "$period/$name"
+            }
+            if (api.listCredentials().contains(name)) {
+                throw DuplicateKeyException()
             }
             api.putCode(name, secret, oathType, algorithm, digits, counter, touch)
             return Credential(deviceInfo.id, name, oathType, touch)

app/src/main/kotlin/com/dangerousthings/vivoauth/exc/DuplicateKeyException.kt
@@ -1,0 +1,5 @@
+package com.dangerousthings.vivoauth.exc
+
+import java.io.IOException
+
+class DuplicateKeyException() : IOException("A Credential with the same name already exists")

app/src/main/kotlin/com/dangerousthings/vivoauth/ui/add/AddCredentialActivity.kt
@@ -12,6 +12,7 @@ import android.view.MenuItem
 import android.widget.ProgressBar
 import com.dangerousthings.vivoauth.R
 import com.dangerousthings.vivoauth.ui.BaseActivity
+import com.dangerousthings.vivoauth.exc.DuplicateKeyException
 import kotlinx.coroutines.experimental.CancellationException
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.delay
@@ -86,7 +87,9 @@ class AddCredentialActivity : BaseActivity<AddCredentialViewModel>(AddCredential
                                 }
                             })
                             finish()
-                        } catch(e: CancellationException) {
+                        } catch (e: CancellationException) {
+                        } catch (e: DuplicateKeyException) {
+                            markDuplicateName()
                         } catch (e: Exception) {
                             Log.e("yubioath", "exception", e)
                             validateVersion(data, viewModel.lastDeviceInfo.version)