Yubico/yubioath-android

Improve custom icon support.

app/src/main/kotlin/com/yubico/yubioath/ui/settings/SettingsViewModel.kt
@@ -2,19 +2,26 @@ package com.yubico.yubioath.ui.settings
 
 import com.yubico.yubioath.R
 import com.yubico.yubioath.ui.BaseViewModel
+import com.yubico.yubioath.ui.main.IconManager
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.toast
 
-/**
- * Created by Dain on 2017-08-15.
- */
 class SettingsViewModel : BaseViewModel() {
     fun clearStoredPasswords() {
-        services!!.let {
+        services?.let {
             it.keyManager.clearAll()
             launch(UI) {
-                it.context.toast(R.string.data_cleared)
+                it.context.toast(R.string.passwords_cleared)
+            }
+        }
+    }
+
+    fun clearIcons() {
+        services?.let {
+            IconManager(it.context).clearIcons()
+            launch(UI) {
+                it.context.toast(R.string.icons_cleared)
             }
         }
     }

VivoKey/vivokey-authenticator

app/src/main/kotlin/com/dangerousthings/vivoauth/ui/settings/SettingsViewModel.kt
@@ -2,19 +2,26 @@ package com.dangerousthings.vivoauth.ui.settings
 
 import com.dangerousthings.vivoauth.R
 import com.dangerousthings.vivoauth.ui.BaseViewModel
+import com.dangerousthings.vivoauth.ui.main.IconManager
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.toast
 
-/**
- * Created by Dain on 2017-08-15.
- */
 class SettingsViewModel : BaseViewModel() {
     fun clearStoredPasswords() {
-        services!!.let {
+        services?.let {
             it.keyManager.clearAll()
             launch(UI) {
-                it.context.toast(R.string.data_cleared)
+                it.context.toast(R.string.passwords_cleared)
+            }
+        }
+    }
+
+    fun clearIcons() {
+        services?.let {
+            IconManager(it.context).clearIcons()
+            launch(UI) {
+                it.context.toast(R.string.icons_cleared)
             }
         }
     }